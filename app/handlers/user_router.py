9864404a17cff72128427a049b134a5a7d348060
WizarPoka <Vasya-nve12401@yandex.ru>
2024-05-03 01:02:19 +0300

    перевел бота на aiogram 3

--- app/handlers/user_router.py
+++ app/handlers/user_router.py
@@ -1,11 +1,24 @@
-import telebot
+# ====================================================================================
+
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 
-# Создаем экземпляр телеграм-бота
-bot = telebot.TeleBot("7152606441:AAEY0jELuQNAiLj9D2Nf50x8npFBq04caBs")
+# import app.handlers.keyboards as kb
+
+user_router = Router()
 
 # Глобальная переменная для отслеживания состояния входа
 is_logged_in = False
@@ -16,14 +29,16 @@ options.add_argument('--disable-extensions')
 options.add_argument('--headless')  # Запуск браузера в фоновом режиме (без графического интерфейса)
 driver = webdriver.Chrome(options=options)
 
+# ====================================================================================
+ 
 # Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот для получения ссылки на игру в GeoGuessr. Используйте команду /go для входа в аккаунт и начала игры. Введите URL карты:")
+@user_router.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Привет! Я бот для получения ссылки на игру в GeoGuessr. Используйте команду /go для входа в аккаунт и начала игры. Введите URL карты:")
 
 # Обработчик команды /go
-@bot.message_handler(commands=['go'])
-def go(message):
+@user_router.message(Command("go"))
+async def go(message: Message):
     global is_logged_in
     if not is_logged_in:
         # Заходим на страницу входа
@@ -45,24 +60,24 @@ def go(message):
 
         time.sleep(1)
         is_logged_in = True
-        bot.send_message(message.chat.id, "Вы успешно вошли в аккаунт.")
+        await message.answer("Вы успешно вошли в аккаунт.")
     else:
-        bot.send_message(message.chat.id, "Вы уже вошли в аккаунт.")
+        await message.answer("Вы уже вошли в аккаунт.")
 
-# Обработчик команды /stop
-@bot.message_handler(commands=['stop'])
-def stop(message):
-    global is_logged_in
-    if is_logged_in:
-        driver.quit()  # Закрываем браузер
-        is_logged_in = False
-        bot.send_message(message.chat.id, "Вы успешно вышли из аккаунта и закрыли браузер.")
-    else:
-        bot.send_message(message.chat.id, "Вы еще не вошли в аккаунт.")
+# # Обработчик команды /stop
+# @user_router.message_handler(commands=['stop'])
+# async def stop(message: Message):
+#     global is_logged_in
+#     if is_logged_in:
+#         driver.quit()  # Закрываем браузер
+#         is_logged_in = False
+#         user_router.send_message(message.chat.id, "Вы успешно вышли из аккаунта и закрыли браузер.")
+#     else:
+#         user_router.send_message(message.chat.id, "Вы еще не вошли в аккаунт.")
 
 # Обработчик всех текстовых сообщений
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+@user_router.message()
+async def echo_all(message: Message):
     global is_logged_in
     if is_logged_in:
         map_url = message.text
@@ -82,10 +97,15 @@ def echo_all(message):
         link_field = driver.find_element("name", "copy-link")
         link_value = link_field.get_attribute("value")
 
-        bot.send_message(message.chat.id, link_value)
+        await message.answer(link_value)
     else:
-        bot.send_message(message.chat.id, "Сначала войдите в аккаунт с помощью команды /go.")
+        await message.answer("Сначала войдите в аккаунт с помощью команды /go.")
 
-# Запускаем бота
-bot.delete_webhook(drop_pending_updates=True)
-bot.polling()
+
+# ====================================================================================
+
+@user_router.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("Команда /help")
+
+
---
+++ run.py
@@ -0,0 +1,37 @@
+# ====================================================================================
+
+import asyncio
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import BotCommandScopeAllPrivateChats
+
+from dotenv import load_dotenv, find_dotenv
+
+from app.handlers.user_router import user_router
+# from app.common.bot_cmds_list import private
+# ====================================================================================
+
+load_dotenv(find_dotenv())
+
+async def main():
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
+
+    dp.include_router(user_router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=BotCommandScopeAllPrivateChats())
+    # await bot.set_my_commands(commands=private, scope=BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot)
+    
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")  
+
+# ====================================================================================
